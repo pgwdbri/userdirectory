03a85c378fe48189e5f8370b336b0162474a9514
Brianna Shade <bri.krystalle@gmail.com>
2021-04-28 11:54:57 -0400

    header and footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer(){
+    return (<footer className="bg-warning text-center">
+        <h1>Copyright: Brianna Shade</h1>
+        <p>Company</p>
+    </footer>)
+}
+
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return (<header className="bg-warning text-center">
+        <h1>Employee Directory</h1>
+        <p>Company</p>
+    </header>)
+
+
+
+}
+
+export default Header;
