9b0bfc54341620ce1d8bea908a19d195217dc34b
Brianna Shade <bri.krystalle@gmail.com>
2021-04-29 23:07:00 -0400

    updated search bar

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,7 +3,9 @@ import React from "react";
 class Search extends React.Component{
     state = {
         searchItem: '',
+        
     };
+
     getsearchItem = (event) => {
         var usertext = event.target.value
         this.setState({
@@ -14,7 +16,7 @@ class Search extends React.Component{
         return (<div>
             <div className="input-group mb-3">
         <input type="text" value={this.state.searchItem} onChange={this.getsearchItem} placeholder="Search" aria-label="Search" aria-describedby="button-addon2"/>
-        <button className="btn btn-outline-secondary" onClick ={() => {this.props.searchEmp(this.state.searchItem)}}type="button" id="button-addon2">Search Employee by name</button>
+        <button className="btn btn-outline-secondary" onClick ={() => {this.props.searchEmp(this.state.searchItem)}}type="button" id="button-addon2">Search Employee by First and Last Name</button>
       </div></div>)
     }
 }
